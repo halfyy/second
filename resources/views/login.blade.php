45f305451bb298e14ba2857fb3acbc03badecf91
VaDe17 <sasha.potapov.2003@bk.ru>
2018-07-12 10:57:05 +0900

    update, add first page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link type="text/css" href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Rock+Salt|The+Girl+Next+Door" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>WWHelp</title>
+    <link rel="SHORTCUT ICON" href="img/favicon.ico" type="image/gif">
+</head>
+<body>   
+        
+    <div class="row bg-primary align-items-center sticky-top w-100"  id="header">
+        <div class="col col-md-4 offset-4 offset-md-2 offset-lg-1">
+            <h1 class="offset-lg-3" style="color:ghostwhite;" class="col">WWHelp</h1>
+        </div>
+        <div class="col offset-lg-2 hide-on-small-only row">
+            <a href="homepage.html" class="text-light btn col-3">Home</a>
+            <div class="dropdown text-light col-3">
+                <button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    .......
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#!">ss</a>
+                    <a class="dropdown-item" href="#!">ss</a>
+                    <a class="dropdown-item" href="#!">ss</a>
+                </div>
+            </div>
+            <a href="#!" class="text-light btn col-3">Contacts</a>
+        </div>
+    </div>
+    
+    <div class="container mt-5">
+        <div class="card container pt-3">
+            <div class="card-body">
+                <button class="btn-primary btn col-10 offset-1 pt-3 pb-3 btn-circle">
+                    <p class="fs-60">
+                        Register
+                    </p>
+                </button>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script>
+        $('.carousel').carousel({
+            interval: 2000;
+        });
+    </script>
+</body>
+</html>
