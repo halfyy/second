173ea37b40b58bfcfc26d8f791aeafe0b2081079
VaDe17 <sasha.potapov.2003@bk.ru>
2018-07-13 09:55:41 +0900

    change links in header

--- resources/views/mainUser.blade.php
+++ resources/views/mainUser.blade.php
@@ -44,6 +44,24 @@
         </div>
     </div>
     
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-8">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="h1">Hello, World!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col offset-1">
+                <div class="card">
+                    AD's
+                </div>
+            
+            </div>
+        </div>
+    </div>
+    
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
